14106f45d35b6793a0ba28041a97a263eedeb2ed
严俊东 <1172482914@qq.com>
2018-06-08 23:23:03 +0800

    获取基本参数

--- src/pages/content/PostCreate.vue
+++ src/pages/content/PostCreate.vue
@@ -1,25 +1,19 @@
 <template>
 	<div>
 		<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-			<el-form-item label="笔记标题" prop="name">
-				<el-input v-model="ruleForm.name"></el-input>
+			<el-form-item label="笔记标题" prop="title">
+				<el-input v-model="ruleForm.title"></el-input>
 			</el-form-item>
 			<el-form-item label="笔记分类">
-				<el-input v-model="ruleForm.name"></el-input>
+				<el-input v-model="ruleForm.categories"></el-input>
 			</el-form-item>
 			<el-form-item label="笔记标签">
-				<el-input v-model="ruleForm.name"></el-input>
+				<el-input v-model="ruleForm.tags"></el-input>
 			</el-form-item>
 			<el-form-item label="发布时间" required>
 				<el-col :span="11">
 					<el-form-item prop="date1">
 						<el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
-					</el-form-item>
-				</el-col>
-				<el-col class="line" :span="2">-</el-col>
-				<el-col :span="11">
-					<el-form-item prop="date2">
-						<el-time-picker type="fixed-time" placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
 					</el-form-item>
 				</el-col>
 			</el-form-item>
@@ -46,28 +40,22 @@ export default {
 			query: this.$route.query,
 			detail: {}, // 详情
 			ruleForm: {
-				name: '',
-				region: '',
+				title: '',
+				categories: '',
+				tags: '',
 				date1: '',
-				date2: '',
 				delivery: false,
 				type: [],
 				resource: '',
 				desc: ''
 			},
 			rules: {
-				name: [
-					{ required: true, message: '请输入活动名称', trigger: 'blur' },
-					{ min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-				],
-				region: [
-					{ required: true, message: '请选择活动区域', trigger: 'change' }
+				title: [
+					{ required: true, message: '请输入笔记标题', trigger: 'blur' },
+					{ min: 3, max: 5, message: '长度在 3 到 50 个字符', trigger: 'blur' }
 				],
 				date1: [
 					{ type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-				],
-				date2: [
-					{ type: 'date', required: true, message: '请选择时间', trigger: 'change' }
 				],
 				type: [
 					{ type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
@@ -94,9 +82,12 @@ export default {
 		// 数据初始化
 		initData() {
 			if (this.status == this.MODIFY) { // 编辑
-				this.$API.getPost({id: this.query.id}).then(res => {
-					console.log(res.data)
-					this.detail = res.data
+				this.$API.getPost({ id: this.query.id }).then(res => {
+					let detail = res.data
+					this.ruleForm.title = detail.title
+					this.ruleForm.categories = detail.categories.join(',')
+					this.ruleForm.tags = detail.tags.join(',')
+					this.ruleForm.date1 = detail.date
 				})
 			} else { // 创建
 
